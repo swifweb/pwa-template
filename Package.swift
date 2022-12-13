3e30dd9d316a23fd4d131315b7046650a36be1ed
Mihael Isaev <isaev.mihael@gmail.com>
2022-12-14 00:46:21 +0300

    📦 Bump `web` version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 
 import PackageDescription
 
@@ -12,7 +12,7 @@ let package = Package(
         .executable(name: "Service", targets: ["Service"])
     ],
     dependencies: [
-        .package(url: "https://github.com/swifweb/web", from: "1.0.0-beta.1.0.0")
+        .package(url: "https://github.com/swifweb/web", from: "1.0.0-beta.1.19.0")
     ],
     targets: [
         .target(name: "App", dependencies: [
